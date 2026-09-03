5083ab18470fedd93abc8556d83c265a147904f2
kavindu ishara <kavinduishara2923@gmail.com>
2026-09-03 11:23:14 +0530

    add top bar items

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,10 +1,23 @@
-import { Search,User2 } from "lucide-react";
+import { LayoutDashboard, LayoutDashboardIcon, Plus, Search,User2, Users2 } from "lucide-react";
+
 
 export default function TopBar() {
   return (
     <div className="w-full bg-white">
       <div className="flex items-center h-16 px-6 border-b border-gray-200 justify-between">
-        
+        <div className="flex items-center gap-4">
+          <button className="flex items-center gap-2 text-sm font-semibold text-gray-700 bg-blue-100 rounded-lg px-3 py-2">
+            <LayoutDashboard size={16} />
+            <span>Boards</span>
+          </button>
+          
+          <button className="flex items-center gap-2 text-sm font-semibold text-gray-700 bg-blue-100 rounded-lg px-3 py-2">
+            <Users2 size={16} />
+            <span>Team</span>
+          </button>
+          
+        </div>
+
         <div className="relative w-full max-w-md">
             <Search
               size={16}
@@ -18,11 +31,18 @@ export default function TopBar() {
         </div>
           
         <div className="flex items-center gap-2 w-56 justify-end shrink-0">
+
+          <button className="flex items-center gap-2 text-sm font-bold text-white bg-blue-900 rounded-lg px-3 py-2">
+            <Plus size={16} />
+            <span>Add Card</span>
+          </button>
           
           <div className="w-9 h-9 rounded-full bg-gradient-to-br from-orange-400 to-pink-500 flex items-center justify-center text-white text-xs font-medium ml-1">
             <User2/>
           </div>
+
         </div>
+
       </div>
     </div>
   );
